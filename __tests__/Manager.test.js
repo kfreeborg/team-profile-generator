0101ecd42ecd7c78f71b480ed750864c321ba40e
Kelsey Freeborg <kelsey.freeborg@gmail.com>
2020-11-27 12:51:43 -0700

    manager test working

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,23 @@
+const Manager = require('../lib/Manager');
+const Employee = require('../lib/Employee');
+
+test('officeNumber', () => {
+  const officeNumber = 100;
+  const employee = new Manager('name', 100, 'test@test.com', officeNumber);
+
+  expect(employee.officeNumber).toBe(officeNumber);
+});
+
+test('getRole()', () => {
+  const role = 'Manager';
+  const employee = new Manager('name', 100, 'test@test.com', 100);
+
+  expect(employee.getRole()).toBe(role);
+});
+
+test('get officeNumber()', () => {
+  const officeNumber = 100;
+  const employee = new Manager('name', 100, 'test@test.com', officeNumber);
+
+  expect(employee.getOfficeNumber()).toBe(officeNumber);
+});
